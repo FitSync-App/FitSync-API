e54a0f3f703333ace30788e1c0458142793a7dd9
Oscar Kaluza <oscarkaluza1@gmail.com>
2024-03-12 10:23:48 +0100

    -

--- WorkoutApp/Models/ExerciseEquipment.cs
+++ WorkoutApp/Models/ExerciseEquipment.cs
@@ -3,10 +3,11 @@
     public class ExerciseEquipment
     {
         public int Id { get; set; }
-        public int ExerciseId { get; set; }
-        public int EquipmentId { get; set; }
-        public Exercise Exercise { get; set; }
-        public Equipment Equipment { get; set; }    
 
+        public int ExerciseId { get; set; }
+        public Exercise Exercise { get; set; }
+
+        public int EquipmentId { get; set; }
+        public Equipment Equipment { get; set; }
     }
 }
