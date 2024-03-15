e4c0ca5c9740e4b81ebc7b543127fc93738e31ac
osck53 <osck53@OSCARKALUZA6258>
2024-03-15 12:28:11 +0100

    Cors policy modified + Exercise properties added.

--- WorkoutApp/Models/Exercise.cs
+++ WorkoutApp/Models/Exercise.cs
@@ -6,7 +6,12 @@
         public string Name { get; set; }
         public string Difficulty { get; set; }
         public string Description { get; set; }
+
+        public int? EquipmentId { get; set; }
+        public int? MuscleId { get; set; }
+
         public Equipment Equipment { get; set; }
         public Muscle Muscle { get; set; }
     }
+
 }
--- WorkoutApp/Program.cs
+++ WorkoutApp/Program.cs
@@ -2,28 +2,36 @@ using Fitsync.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin()
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DatabaseContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("DatabaseConnection")));
 
-builder.Configuration.GetConnectionString("DatabaseConnection");
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
+
 
 app.UseAuthorization();
 
